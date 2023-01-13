5b59e833887ea2591508116f0cc4f50224269129
Isaac <isaac.lin.pnc@gmail.com>
2023-01-13 14:06:29 -0500

    finished ToDo list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    handleToDo(e.target.querySelector('#new-task-description').value)
+    form.reset()
+  })
 });
+
+function handleToDo(todo) {
+  li = document.createElement('li');
+  btn = document.createElement('button');
+  btn.addEventListener("click", handleDelete)
+  li.textContent = `${todo} `;
+  btn.textContent = 'x'
+  li.appendChild(btn);
+  document.querySelector('#tasks').appendChild(li)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove();
+}
